d389e655f604bb45de2b6de003184d5364bcfd37
V1tale <elativ.c@gmail.com>
2022-10-07 14:33:23 +0200

    inizializzazione

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// INPUT
+// - Il numero di chilometri da percorrere
+// - Età del passeggero
+
+// PROCESSING
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// - va applicato uno sconto del 20% per i minorenni
+// - va applicato uno sconto del 40% per gli over 65.
+// OUTPUT
