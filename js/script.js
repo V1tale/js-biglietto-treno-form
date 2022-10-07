1a8068a76df765f46580e1fc64d6aa854d6c4bfa
V1tale <elativ.c@gmail.com>
2022-10-07 15:42:07 +0200

    computing

--- js/script.js
+++ js/script.js
@@ -20,6 +20,13 @@ const discountY = bill * 0.8;
 console.log(discountY, typeof discountY);
 const discountE = bill * 0.6;
 console.log(discountE, typeof discountE);
+// COMPUTING
+let finalPrice = bill
+    if (userAge < 18)
+        finalPrice = discountY;
+    else if (userAge > 65)
+        finalPrice = discountE
+    console.log(finalPrice, typeof finalPrice);   
 // - il prezzo del biglietto è definito in base ai km (0.21 € al km)
 // - va applicato uno sconto del 20% per i minorenni
 // - va applicato uno sconto del 40% per gli over 65.
