9b91350040c62bce748fbd74c652f3973a2c85cd
V1tale <elativ.c@gmail.com>
2022-10-07 15:05:58 +0200

    input

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 // INPUT
 // - Il numero di chilometri da percorrere
+const kilometresInput = document.getElementById("kilometres");
+const kilometres = parseInt(kilometresInput.value);
+console.log(kilometres, typeof kilometres);
 // - Età del passeggero
-
+const userAgeInput = document.getElementById("userage");
+const userAge = parseInt(userAgeInput.value);
+console.log(userAge, typeof userAge);
 // PROCESSING
 // - il prezzo del biglietto è definito in base ai km (0.21 € al km)
 // - va applicato uno sconto del 20% per i minorenni
