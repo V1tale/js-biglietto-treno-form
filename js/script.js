73a1a2e1dffbfb06d41f27c921e1debf36714306
V1tale <elativ.c@gmail.com>
2022-10-10 17:07:58 +0200

    apportati miglioramenti

--- js/script.js
+++ js/script.js
@@ -1,46 +1,36 @@
 // INPUT
-// - Il numero di chilometri da percorrere
 const kilometresInput = document.getElementById("kilometres");
-const kilometres = parseFloat(kilometresInput.value);
-console.log(kilometresInput, typeof kilometresInput);
-// - Età del passeggero
 const userAgeInput = document.getElementById("userage");
-const userAge = parseInt(userAgeInput.value);
-console.log(userAgeInput, typeof userAgeInput);
-// BUTTON
 const submit = document. getElementById("submit");
 // PROCESSING
-// PRICE
-const price = 0.21;
-const bill = kilometres * 0.21;
-console.log(bill, typeof bill);
-// DISCOUNT
-const discountY = bill * 0.8;
-console.log(discountY, typeof discountY);
-const discountE = bill * 0.6;
-console.log(discountE, typeof discountE);
-// COMPUTING
-let finalPrice = bill
-    if (userAge < 18)
-        finalPrice = discountY;
-    else if (userAge > 65)
-        finalPrice = discountE
-    console.log(finalPrice, typeof finalPrice);  
-
-const priceResult = finalPrice.toFixed(2);
+submit. addEventListener("click", function () {
     
-let discountPerAge = "0"
-if (userAge < 18)
-    discountPerAge = "20%"
-else if (userAge > 65)
-    discountPerAge = "40%"
-
-const ticketNumber = parseInt(Math.random() * (10000));
+    const kilometres = parseFloat(kilometresInput.value);
+    console.log(kilometres, typeof kilometres);
+    const userAge = parseInt(userAgeInput.value);
+    console.log(userAge, typeof userAge);
+    // DISCOUNT
+    let price = kilometres * 0.21;
+    console.log(price, typeof price); 
+    if (userAge < 18)
+    price = price * 0.8;
+    else if (userAge > 65)
+    price = price * 0.6;
+    console.log(price, typeof price);
+    const priceResult = parseFloat(price.toFixed(2));
+    console.log(priceResult, typeof priceResult);
+// DISCOUNT STRING
+    let discountPerAge = "0";
+    if (userAge < 18)
+    discountPerAge = "20%";
+    else if (userAge > 65)
+    discountPerAge = "40%";
+    // TICKET NUMBER
+    const ticketNumber = parseInt(Math.random() * (10000));
 // OUTPUT
- submit. addEventListener("click", function refreshPage() {
- document. getElementById("km").innerHTML = `Chilometri da percorrere: ${kilometres}`;
-document. getElementById("age").innerHTML = `La tua età: ${userAge}`;
-document. getElementById("finalbill").innerHTML = `Il prezzo: € ${priceResult}`
-document. getElementById ("discount"). innerHTML = `Sconto applicato ${discountPerAge}`
-document. getElementById ("ticket") .innerHTML = `il numero del tuo biglietto: ${ticketNumber}`      
+    document. getElementById("km").innerHTML = `Chilometri da percorrere: ${kilometres}`;
+    document. getElementById("age").innerHTML = `La tua età: ${userAge}`;
+    document. getElementById("finalbill").innerHTML = `Il prezzo: € ${priceResult}`;
+    document. getElementById ("discount"). innerHTML = `Sconto applicato ${discountPerAge}`;
+    document. getElementById ("ticket") .innerHTML = `il numero del tuo biglietto: ${ticketNumber}`;    
 })
